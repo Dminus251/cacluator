ad257f3718885f6edc12b75a9b22a611563ad07c
Dminus251 <yyk0370@naver.com>
2024-07-17 16:56:45 +0900

    delete constant

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class CalculatorApplication {
-    private static final String constant = "constant"; //*****NEW*****
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args); 
     }
