4845e5b836a62285521c3dfed4d510858725bb35
Dminus251 <yyk0370@naver.com>
2024-07-24 12:51:31 +0900

    test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' calcForStaging)
-test $(curl -s "http://$CONTAINER_IP:8081/sum?a=100&b=172") -eq 272
+RESPONSE=$(curl -s "http://$CONTAINER_IP:8081/sum?a=100&b=172")
+echo "CONTAINER_IP: $CONTAINER_IP" 
+echo "Response: $RESPONSE"
+test "$RESPONSE" -eq 272
 
