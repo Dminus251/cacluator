495e7981e2d5c93f3997f429b4fd9ebcb32f366c
Dminus251 <yyk0370@naver.com>
2024-07-24 20:26:19 +0900

    test localhost

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -2,9 +2,9 @@
 
 #curl 결과를 result 변수에 저장
 #result=$(docker exec calcForStaging curl -s "http://localhost:8081/sum?a=1&b=2") - success
-result=$(docker exec calcForStaging curl -s "http://localhost:8765/sum?a=1&b=2")
+#result=$(docker exec calcForStaging curl -s "http://localhost:8765/sum?a=1&b=2") - failure
 #result=$(curl -s "http://localhost:8081/sum?a=1&b=2") - failure
-#result=$(curl -s "http://localhost:8765/sum?a=1&b=2") - failure....?
+result=$(curl -s "http://localhost:8765/sum?a=1&b=2")
 echo "$result"
 # 기대값을 설정: 100+172=272
 expected=3
