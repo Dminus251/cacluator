f4a8be07351299027fc0f0fa596c0c502f09fb93
Dminus251 <yyk0370@naver.com>
2024-07-24 13:56:47 +0900

    Dockerfile에서 apt update 제거, 인수 테스트.sh 주석 제거

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# curl을 통해 컨테이너 내부에서 HTTP 요청을 보내고, 그 결과를 변수 result에 저장합니다.
+#curl 결과를 result 변수에 저장
 result=$(docker exec calcForStaging curl -s "http://localhost:8081/sum?a=100&b=172")
 
-# 기대값을 설정합니다.
+# 기대값을 설정: 100+172=272
 expected=272
 
-# 결과값이 기대값과 동일한지 비교합니다.
+# 결과값이 기대값과 동일한지 체크
 if [ "$result" -eq "$expected" ]; then
-    echo "Test passed: $result equals $expected"
+    exit 0 #pass
 else
     echo "Test failed: $result does not equal $expected"
     exit 1
