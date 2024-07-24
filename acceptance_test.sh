16aceb13210d1b38f5a5a177da0176bdb808f8fa
Dminus251 <yyk0370@naver.com>
2024-07-24 12:42:55 +0900

    change to using container ip

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-test $(curl "http://calcForStaging:8081/sum?a=100&b=172") -eq 272
+CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' calcForStaging)
+test $(curl -s "http://$CONTAINER_IP:8081/sum?a=100&b=172") -eq 272
 
